52ba3b5a504d80fa550991de958d2d974884d0d9
mtaketani113 <47836003+mtaketani113@users.noreply.github.com>
2024-01-24 00:53:34 +0900

    初期作成

---
+++ src/components/Home.tsx
@@ -0,0 +1,17 @@
+/**
+ * HomeのJSX
+ *
+ * @param param0
+ * @returns
+ */
+const Home = () => {
+  return (
+    <>
+        <li>
+          <a href="./RPG">PRG</a>
+        </li>
+    </>
+  );
+};
+
+export default Home;
---
+++ src/components/RPG.tsx
@@ -0,0 +1,15 @@
+/**
+ * RPG
+ *
+ * @param param0
+ * @returns
+ */
+const RPG = () => {
+    return (
+      <>
+        <canvas id="main"></canvas>
+      </>
+    );
+  };
+  
+  export default RPG;
